0b867610ec6999882db2c148cb3cd94030bd4900
Siranjeevi p <siranjeevi.p@initbit.in>
2022-02-12 23:14:14 +0530

    add alt tag in img

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import Styles from './Banner.module.scss'
 import BannerImg from '../../assets/img/bg1.png'
 const Banner=()=> {
     return <div className={Styles.BannerWrapper} >
-        <img src={BannerImg} alt="iyal Banner image"/>
+        <img src={BannerImg} alt="Banner Product" />
     </div>
 }
 export default Banner;
