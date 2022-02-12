faa9ab34e61b32d2fe43147db05bb3f90526537c
Siranjeevi p <siranjeevi.p@initbit.in>
2022-02-12 23:04:45 +0530

    build issue fixes

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,8 +1,8 @@
 import Styles from './Banner.module.scss'
 import BannerImg from '../../assets/img/bg1.png'
 const Banner=()=> {
-    return <div className={Styles.BannerWrapper} alt="iyal Banner image">
-        <img src={BannerImg} />
+    return <div className={Styles.BannerWrapper} >
+        <img src={BannerImg} alt="iyal Banner image"/>
     </div>
 }
 export default Banner;
